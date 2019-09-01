6145d34872315ba0ea4818dbee4ae90004c69f37
Sapfir0 <sapfir999999@yandex.ru>
2019-09-01 18:50:51 +0400

    Пусть будет еще и это

--- src/services/esp8266specialize/sleeping.h
+++ src/services/esp8266specialize/sleeping.h
@@ -5,4 +5,11 @@
 #ifndef METEOSTATION_SLEEPING_H
 #define METEOSTATION_SLEEPING_H
 
+#include <ESP8266WiFi.h>
+
+void deepSleep(uint milliseconds) {
+    uint mcSeconds = pow(milliseconds, 3);
+    ESP.deepSleep(mcSeconds);
+}
+
 #endif //METEOSTATION_SLEEPING_H
